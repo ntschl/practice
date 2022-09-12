1b5c6867b76bc0af0ca05d29ea65b09842de20f4
Nathaniel Schlie <schlie.nt@c02w471whtd6.localdomain>
2022-09-12 14:39:14 -0400

    pushing final slice sums

--- arrays and slices/sum.go
+++ arrays and slices/sum.go
@@ -17,3 +17,18 @@ func SumAll(nums ...[]int) []int {
 
 	return sums
 }
+
+func SumAllTails(nums ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range nums {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}
