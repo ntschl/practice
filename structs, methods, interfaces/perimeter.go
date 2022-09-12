58fa86c662c6ff4c559b9b6cea47f8a49bff4ffd
Nathaniel Schlie <schlie.nt@c02w471whtd6.localdomain>
2022-09-12 16:08:12 -0400

    pushing structs methods interfaces

---
+++ structs, methods, interfaces/perimeter.go
@@ -0,0 +1,37 @@
+package perimeter
+
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
+type Rectangle struct {
+	Length float64
+	Width  float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Length
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Length + rectangle.Width)
+}
---
+++ structs, methods, interfaces/perimeter_test.go
@@ -0,0 +1,36 @@
+package perimeter
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+
+	areaTests := []struct { //declare a slice of structs
+		name    string  //test name
+		shape   Shape   //shape under test
+		hasArea float64 //name of 'want'
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Length: 6}, hasArea: 72.0}, //fill test slice with structs
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
+	}
+
+	for _, tt := range areaTests { //iterate over slice of tests
+		// using tt.name from the case to use it as the `t.Run` test name
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
+	}
+}
