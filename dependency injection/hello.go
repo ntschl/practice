fd4c96587875a658777fa168b3cd32b10585aa05
Nathaniel Schlie <schlie.nt@c02w471whtd6.localdomain>
2022-09-23 10:45:04 -0400

    pushing concurrency

--- dependency injection/hello.go
+++ dependency injection/hello.go
@@ -3,8 +3,13 @@ package main
 import (
 	"fmt"
 	"io"
+	"os"
 )
 
 func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func main() {
+	Greet(os.Stdout, "Nate")
 }
